9e309789a10901b7a6b96afbe80b6d665e8babf4
Amh <abaams78@gmail.com>
2017-11-17 09:35:47 -0600

    statement and transactionservice, principal

--- src/main/java/com/lucy/service/TransactionService.java
+++ src/main/java/com/lucy/service/TransactionService.java
@@ -1,8 +1,10 @@
 package com.lucy.service;
 
+import java.sql.Date;
 import java.util.List;
 
 import com.lucy.domain.Transaction;
+import com.lucy.domain.TransactionType;
 
 public interface TransactionService {
 
@@ -10,7 +12,10 @@ public interface TransactionService {
 	public List<Transaction> getAllTransaction();
 	public Transaction save(Transaction transaction);
 	public Transaction update(Transaction transaction);
-	public boolean delete(Transaction transaction);
-	public Transaction createTransaction(Integer accNo, double amount, String description);
+	public void delete(Transaction transaction);
+	public Transaction createTransaction(Integer accNo, double amount, String description,  TransactionType type);
 	public List<Transaction> getTransactionByUser(String username);
+	public Transaction getByDate(Date date);
+	public Transaction getByType(TransactionType type);
+	public List<Transaction> getByAccountNumberAndDates(long accId,Date startDate, Date endDate);
 }
--- src/main/java/com/lucy/validator/test.java
+++ src/main/java/com/lucy/validator/test.java
@@ -1,5 +1,9 @@
 package com.lucy.validator;
 
-public class test {
+import java.util.Calendar;
 
+public class test {
+	public static void main(String[] args) {
+		System.out.println(new java.sql.Date(Calendar.getInstance().getTimeInMillis()));
+	}
 }
