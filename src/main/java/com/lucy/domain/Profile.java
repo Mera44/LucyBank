3d5bc6d4333e9c081a3bf8a1eb54b3274abcdab7
filiboy03 <filiboy03@gmail.com>
2017-11-20 13:56:45 -0600

    profile confirm password

--- src/main/java/com/lucy/domain/Profile.java
+++ src/main/java/com/lucy/domain/Profile.java
@@ -7,82 +7,88 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-
+import javax.persistence.Transient;
 import javax.validation.Valid;
-import javax.validation.constraints.Null;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import com.lucy.validator.EqualPasswords;
+
 //import com.lucy.validator.ProfileId;
 
+@EqualPasswords
 @Entity
 public class Profile {
-	 
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	 private Long id;
-	 
-	 @NotEmpty
-	 @Size(min=2 ,max=50,message="{Size.name.validation}")
-	 private String firstName;
-	 
-	 @NotEmpty
-	 @Size(min=2 ,max=50,message="{Size.name.validation}")
-	 private String lastName;
-	 
-	 @NotEmpty
-	 @Size(min=4 ,max=50,message="{message.name.validation}" )
-	 private String userName;
-	 
-	 @NotEmpty
-	 private String password;
-	 
-	/* //@NotEmpty
-	 @Transient
-	 private String confirmpassword;*/
-	 
-	 @NotEmpty
-	 @Email
-	 private String email;
-	 
-	/* private String userStatus;
-	 private int isActive;*/
-	 
-	/*@NotNull
-	@Past
-	@DateTimeFormat(pattern = "dd/MM/yyyy")
-    private Date birthdate;*/
-	
-	 @Valid
-	 @OneToOne(cascade=CascadeType.ALL)
-	 @JoinColumn
-	 private Address address;
-	 
+	private Long id;
 
-	 @OneToOne(cascade=CascadeType.ALL)
-	 @JoinColumn
-	 private Role role;
+	@NotEmpty
+	@Size(min = 2, max = 50, message = "{Size.name.validation}")
+	private String firstName;
 
-	 
-	 public Profile() {
-			
-		}
+	@NotEmpty
+	@Size(min = 2, max = 50, message = "{Size.name.validation}")
+	private String lastName;
 
+	@NotEmpty
+	@Size(min = 4, max = 50, message = "{message.name.validation}")
+	private String userName;
 
+	@NotEmpty
+	private String password;
+
+	public String getConfirmpassword() {
+		return confirmpassword;
+	}
+
+	public void setConfirmpassword(String confirmpassword) {
+		this.confirmpassword = confirmpassword;
+	}
+
+	// @NotEmpty
+	@Transient
+	private String confirmpassword;
+
+	@NotEmpty
+	@Email
+	private String email;
+
+	/*
+	 * /* private String userStatus; private int isActive;
+	 */
+
+	/*
+	 * @NotNull
+	 * 
+	 * @Past
+	 * 
+	 * @DateTimeFormat(pattern = "dd/MM/yyyy") private Date birthdate;
+	 */
+
+	@Valid
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn
+	private Address address;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn
+	private Role role;
+
+	public Profile() {
+
+	}
 
 	public Long getId() {
 		return id;
 	}
 
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
 
 	public String getFirstName() {
 		return firstName;
@@ -116,10 +122,6 @@ public class Profile {
 		this.password = password;
 	}
 
-
-
-	
-
 	public Role getRole() {
 		return role;
 	}
@@ -136,14 +138,11 @@ public class Profile {
 		this.email = email;
 	}
 
-/*	public Date getBirthdate() {
-		return birthdate;
-	}
-
-	public void setBirthdate(Date birthdate) {
-		this.birthdate = birthdate;
-	}
-*/
+	/*
+	 * public Date getBirthdate() { return birthdate; }
+	 * 
+	 * public void setBirthdate(Date birthdate) { this.birthdate = birthdate; }
+	 */
 	public Address getAddress() {
 		return address;
 	}
@@ -151,9 +150,5 @@ public class Profile {
 	public void setAddress(Address address) {
 		this.address = address;
 	}
-	
-	
-	
 
-	
 }
